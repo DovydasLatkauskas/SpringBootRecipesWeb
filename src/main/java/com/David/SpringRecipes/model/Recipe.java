f3f708e004870d044d48c85d3b2fee5a28b9cb16
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-03-10 21:06:28 +0000

    created get, delete and post methods (without validation)

--- src/main/java/com/David/SpringRecipes/model/Recipe.java
+++ src/main/java/com/David/SpringRecipes/model/Recipe.java
@@ -3,28 +3,79 @@ package com.David.SpringRecipes.model;
 import java.util.List;
 
 public class Recipe {
-    private Integer id;
-    private String name;
+    private String name; // name will be unique and used as id instead of an integer id in order to make linking to recipes nicer
     private String author;
     private List<String> tags;
-    private int prepTime;
-    private int cookTime;
+    private Integer prepTimeMinutes;
+    private Integer cookTimeMinutes;
     private List<String> ingredients;
     private List<String> directions;
     // public Recipe(){    }
-    public Recipe(String name, String author, List<String> tags, int prepTime, int cookTime,
+    public Recipe(String name, String author, List<String> tags, Integer prepTimeMinutes, Integer cookTimeMinutes,
                   List<String> ingredients, List<String> directions) {
-        this.name = name;
+        this.name = name; //TODO check if no recipe is called {name} already
         this.author = author;
         this.tags = tags;
-        this.prepTime = prepTime;
-        this.cookTime = cookTime;
+        this.prepTimeMinutes = prepTimeMinutes;
+        this.cookTimeMinutes = cookTimeMinutes;
         this.ingredients = ingredients;
         this.directions = directions;
     }
 
-    public Recipe get(){
-
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        //TODO check if no recipe is called {name} already
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Integer getPrepTimeMinutes() {
+        return prepTimeMinutes;
+    }
+
+    public void setPrepTimeMinutes(Integer prepTimeMinutes) {
+        this.prepTimeMinutes = prepTimeMinutes;
+    }
+
+    public Integer getCookTimeMinutes() {
+        return cookTimeMinutes;
+    }
+
+    public void setCookTimeMinutes(Integer cookTimeMinutes) {
+        this.cookTimeMinutes = cookTimeMinutes;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(List<String> directions) {
+        this.directions = directions;
+    }
 }
--- src/main/java/com/David/SpringRecipes/web/SpringRecipesController.java
+++ src/main/java/com/David/SpringRecipes/web/SpringRecipesController.java
@@ -1,20 +1,54 @@
 package com.David.SpringRecipes.web;
 
 import com.David.SpringRecipes.model.Recipe;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class SpringRecipesController {
-    private List<Recipe> db;
+    // for testing purposes only
+    Recipe testRecipe1 = new Recipe("Pasta", "Peter", new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata")), 5, 10,
+            new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata")), new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata")));
+    Recipe testRecipe2 = new Recipe("Lasagna", "Garfield", new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata")), 5, 10,
+            new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata")), new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata")));
+    private Map<String, Recipe> db = new HashMap<>(){{
+        put("Pasta", testRecipe1);
+        put("Lasagna", testRecipe2);
+    }};
+
 
     @GetMapping("/")
     public String hello(){
         return "Hello world!";
     }
+    @GetMapping("/recipes")
+    public Collection<Recipe> get() {
+        return db.values();
+    }
+    @GetMapping("/recipes/{name}")
+    public Recipe getByName(@PathVariable String name) {
+        Recipe recipe = db.get(name);
+        if(recipe == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return recipe;
+    }
+    @DeleteMapping("/recipes/{name}")
+    public void delete(@PathVariable String name){
+        Recipe recipe = db.remove(name);
+        if(recipe == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/recipes/")
+    public Recipe create(@RequestBody Recipe recipe){
+        db.put(recipe.getName(), recipe);
+        return recipe;
+    }
 
-    @GetMapping("/{name}")
-    public Recipe get(){ return recipeService.get()}
+    //@GetMapping("/{name}")
+    //public Recipe get(){ return recipeService.get();}
 }
