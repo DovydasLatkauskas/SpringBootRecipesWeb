c1061f6bee08f7168c1b437a2b0009ae7a57b352
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-03-13 19:53:35 +0000

    got thymeleaf to finally work

--- src/main/java/com/David/SpringRecipes/web/SpringRecipesController.java
+++ src/main/java/com/David/SpringRecipes/web/SpringRecipesController.java
@@ -3,22 +3,24 @@ package com.David.SpringRecipes.web;
 import com.David.SpringRecipes.model.Recipe;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
-@RestController
+//@RestController
+@Controller
 public class SpringRecipesController {
     // for testing purposes only
     Recipe testRecipe1 = new Recipe("Pasta", "Peter", new String[]{"Buenos Aires", "Córdoba", "La Plata"}, 5, 10,
             new String[]{"Buenos Aires", "Córdoba", "La Plata"}, new String[]{"Buenos Aires", "Córdoba", "La Plata"});
     Recipe testRecipe2 = new Recipe("Lasagna", "Garfield", new String[]{"Buenos Aires", "Córdoba", "La Plata"}, 5, 10,
             new String[]{"Buenos Aires", "Córdoba", "La Plata"}, new String[]{"Buenos Aires", "Córdoba", "La Plata"});
-    private Map<String, Recipe> db = new HashMap<>(){{
-        put("Pasta", testRecipe1);
-        put("Lasagna", testRecipe2);
+    private ArrayList<Recipe> db = new ArrayList<Recipe>(){{
+        add(testRecipe1);
+        add(testRecipe2);
     }};
 
 
@@ -28,34 +30,34 @@ public class SpringRecipesController {
     }
     @GetMapping("/all-recipes")
     public Collection<Recipe> get() {
-        return db.values();
+        return db;
     }
-    @GetMapping("/recipes/{name}")
-    public Recipe getByName(@PathVariable String name) {
-        Recipe recipe = db.get(name);
-        if(recipe == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-        return recipe;
-    }
+//    @GetMapping("/recipes/{name}")
+//    public Recipe getByName(@PathVariable String name) {
+//        Recipe recipe = db.get(name);
+//        if(recipe == null){
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//        }
+//        return recipe;
+//    }
 
     @GetMapping("/recipes")
     public String recipes(Model model) {
-        Collection<Recipe> recipes = db.values();
+        ArrayList<Recipe> recipes = db;
         model.addAttribute("recipes", recipes);
         return "recipes-list";
     }
 
-    @DeleteMapping("/recipes/{name}")
-    public void delete(@PathVariable String name){
-        Recipe recipe = db.remove(name);
-        if(recipe == null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-    }
+//    @DeleteMapping("/recipes/{name}")
+//    public void delete(@PathVariable String name){
+//        Recipe recipe = db.remove(name);
+//        if(recipe == null){
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//        }
+//    }
     @PostMapping(value = "/recipes/submit")
     public Recipe create(@RequestBody @Valid Recipe recipe){
-        db.put(recipe.getName(), recipe);
+        db.add(recipe);
         return recipe;
     }
 }
